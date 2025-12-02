912b92516c3eb94d83191d5c0efc0e6b9e38edd5
Sapir1913 <shohamsapir221@gmail.com>
2025-12-02 18:17:08 +0200

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -92,10 +92,6 @@ public class Encryption
         
     }
     
-    public static void main(String[] args){
-        
-        System.out.println("Welcome to the Encryption / Decryption Program");
-        
-    }
+   
     
 }
