9138d724203f74a034eca7c82989ffd28ee75132
Sapir1913 <shohamsapir221@gmail.com>
2025-12-02 18:12:19 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -10,6 +10,113 @@ import org.junit.Test;
  */
 public class EncryptionTester
 {
+   //shoham/s part
+   @Test
+    public void testAdd(){
+        
+        int result = Encryption.countWords("hello world java");
+        assertEquals(3,result);
+        
+    }
+    @Test
+    public void testAdd1(){
+        
+        int result = Encryption.countWords("ultras haifa");
+        assertEquals(2,result);
+        
+    }
+    @Test
+    public void testAdd2(){
+        
+        int result = Encryption.countWords("givat shmuel");
+        assertEquals(2,result);
+        
+    }
+    @Test
+    public void testAdd3(){
+        
+        int result = Encryption.countWords("maccabi");
+        assertEquals(1,result);
+        
+    }
+    @Test
+    public void testAdd4(){
+        
+        int result = Encryption.countWords("shoham sapir");
+        assertEquals(2,result);
+        
+    }
+    @Test
+    public void testAdd5(){
+        
+        String result = Encryption.shiftWordsRight("hello world java", 3);
+        assertEquals("java hello world",result);
+        
+    }
+    @Test
+    public void testAdd6(){
+        
+        String result = Encryption.shiftWordsRight("hello world", 2);
+        assertEquals("world hello",result);
+        
+    }
+    @Test
+    public void testAdd7(){
+        
+        String result = Encryption.shiftWordsRight("java", 1);
+        assertEquals("java",result);
+        
+    }
+    @Test
+    public void testAdd8(){
+        
+        String result = Encryption.shiftWordsRight("maccabi haifa ultras", 3);
+        assertEquals("ultras maccabi haifa",result);
+        
+    }
+    @Test
+    public void testAdd9(){
+        
+        String result = Encryption.shiftWordsRight("world", 1);
+        assertEquals("world",result);
+        
+    }
+    @Test
+    public void testAdd10(){
+        
+        String result = Encryption.shiftWordsLeft("java hello world", 3);
+        assertEquals("hello world java",result);
+        
+    }
+     @Test
+    public void testAdd11(){
+        
+        String result = Encryption.shiftWordsLeft("shoham sapir", 2);
+        assertEquals("sapir shoham",result);
+        
+    }
+     @Test
+    public void testAdd12(){
+        
+        String result = Encryption.shiftWordsLeft("izakson noam", 2);
+        assertEquals("noam izakson",result);
+        
+    }
+     @Test
+    public void testAdd13(){
+        
+        String result = Encryption.shiftWordsLeft("yarok ole", 2);
+        assertEquals("ole yarok",result);
+        
+    }
+     @Test
+    public void testAdd14(){
+        
+        String result = Encryption.shiftWordsLeft("java", 1);
+        assertEquals("java",result);
+        
+    }
+   //izakson/s part
    @Test
     public void testAdd(){
         
