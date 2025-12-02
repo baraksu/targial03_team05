3584e9d2c625d0c496e06efffd3cbeeaf34dda68
Sapir1913 <shohamsapir221@gmail.com>
2025-12-02 18:14:03 +0200

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -6,6 +6,72 @@
  */
 public class Encryption
 {
+    public class Encryption \\ shoham's part
+{
+        public static void main(String[] args){
+        
+            System.out.println("Welcome to the Encryption / Decryption Program");
+        
+            }
+    
+        public static int countWords (String sentence) 
+        { // Shoham בדיקת מספר מילים
+            int orgLength = sentence.length();
+            String noSpaces = sentence.replace(" ", "");
+            int newLength = noSpaces.length();
+            int numWords = orgLength - newLength + 1;
+            return numWords;
+        }
+        public static String shiftWordsRight(String sentence, int numWords) 
+        { // Shoham הזזת מילים ימינה
+
+            if (numWords == 1) {
+            return sentence;
+            }
+
+            int firstSpace = sentence.indexOf(" ");
+            int lastSpace = sentence.lastIndexOf(" ");
+
+            if (numWords == 2) {
+            String word1 = sentence.substring(0, firstSpace);
+            String word2 = sentence.substring(firstSpace + 1);
+            return word2 + " " + word1;
+            }
+            if (numWords == 3) {
+            String word1 = sentence.substring(0, firstSpace);
+            String word2 = sentence.substring(firstSpace + 1, lastSpace);
+            String word3 = sentence.substring(lastSpace + 1);
+            return word3 + " " + word1 + " " + word2;
+            }
+            return sentence;
+        }
+        public static String shiftWordsLeft(String sentence, int numWords) 
+        { // Shoham הזזת מילים שמאלה
+
+            if (numWords == 1) {
+            return sentence;
+            }
+
+            int firstSpace = sentence.indexOf(" ");
+            int lastSpace = sentence.lastIndexOf(" ");
+
+            if (numWords == 2) {
+    
+            String word1 = sentence.substring(0, firstSpace);
+            String word2 = sentence.substring(firstSpace + 1);
+            return word2 + " " + word1;
+            }
+
+            if (numWords == 3) {
+
+            String word1 = sentence.substring(0, firstSpace);
+            String word2 = sentence.substring(firstSpace + 1, lastSpace);
+            String word3 = sentence.substring(lastSpace + 1);
+            return word2 + " " + word3 + " " + word1;
+            }
+            return sentence;
+        }
+        }
     //izakson's part: change the original vowels to signs.
     public static String getNewVowels(String sentence) {
             sentence = sentence.replace('u', '&');
