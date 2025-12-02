dfaa6adeb43d09ad164594ef7f849d6b524af456
Noamizaksohn <noam@synergy38.co.il>
2025-12-02 16:54:59 +0200

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -6,12 +6,30 @@
  */
 public class Encryption
 {
+    //izakson's part: change the original vowels to signs.
+    public static String getNewVowels(String sentence) {
+            sentence = sentence.replace('u', '&');
+            sentence = sentence.replace('o', '0');
+            sentence = sentence.replace('i', '1');
+            sentence = sentence.replace('e', '#');
+            sentence = sentence.replace('a', '@');
+            return sentence;
+        }
+    //izakson's part: change the signs to original vowels.
+     public static String getOriginalVowels(String sentence) {
+            sentence = sentence.replace('&', 'u');
+            sentence = sentence.replace('0', 'o');
+            sentence = sentence.replace('1', 'i');
+            sentence = sentence.replace('#', 'e');
+            sentence = sentence.replace('@', 'a');
+            return sentence;
+        
+    }
+    
     public static void main(String[] args){
         
         System.out.println("Welcome to the Encryption / Decryption Program");
         
     }
-    public static int add (int a, int b){
-        return a+b;
-    }
+    
 }
