e3b82806a7424e638cbe97147cfe8d5d48f4f388
Sapir1913 <shohamsapir221@gmail.com>
2025-12-02 18:24:27 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -118,70 +118,70 @@ public class EncryptionTester
     }
    //izakson/s part
    @Test
-    public void testAdd(){
+    public void testAdd15(){
         
         String result = Encryption.getNewVowels("sentence");
         assertEquals("s#nt#nc#",result);
         
     }
     @Test
-    public void testAdd1(){
+    public void testAdd16(){
         
         String result = Encryption.getNewVowels("my name is");
         assertEquals("my n@m# 1s",result);
         
     }
     @Test
-    public void testAdd2(){
+    public void testAdd17(){
         
         String result = Encryption.getNewVowels("i should");
         assertEquals("1 sh0&ld",result);
         
     }
     @Test
-    public void testAdd3(){
+    public void testAdd18(){
         
         String result = Encryption.getNewVowels("nevo");
         assertEquals("n#v0",result);
         
     }
     @Test
-    public void testAdd4(){
+    public void testAdd19(){
         
         String result = Encryption.getNewVowels("shoham");
         assertEquals("sh0h@m",result);
         
     }
     @Test
-    public void testAdd5(){
+    public void testAdd20(){
         
         String result = Encryption.getOriginalVowels("s#nt#nc#");
         assertEquals("sentence",result);
         
     }
     @Test
-    public void testAdd6(){
+    public void testAdd21(){
         
         String result = Encryption.getOriginalVowels("my n@m# 1s");
         assertEquals("my name is",result);
         
     }
     @Test
-    public void testAdd7(){
+    public void testAdd22(){
         
         String result = Encryption.getOriginalVowels("1 sh0&ld");
         assertEquals("i should",result);
         
     }
     @Test
-    public void testAdd8(){
+    public void testAdd23(){
         
         String result = Encryption.getOriginalVowels("n#v0");
         assertEquals("nevo",result);
         
     }
     @Test
-    public void testAdd9(){
+    public void testAdd24(){
         
         String result = Encryption.getOriginalVowels("sh0h@m");
         assertEquals("shoham",result);
